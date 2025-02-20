63354f534cd434ad5d2564c8e325dc5e0ec73ed9
Thevindu Warnakulasuriya <13820898+RebelFinatic@users.noreply.github.com>
2025-02-21 04:17:40 +1100

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "fs_taxes"
+description "A highly customizable taxation script"
+author "RebelFinatic"
+version "1.0.0"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+lua54 'yes'
